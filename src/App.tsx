00096305a3be02501b51194a5ac65743406124c1
Matbilekk <matbilek@gmail.com>
2023-09-27 14:34:31 +0300

    1-st

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Git>Git</Git>
     </div>
   );
 }
+
+const Git = styled.button`
+  color: lightblue;
+  border-radius: 8px;
+  font-size: 3rem;
+  border-color: green;
+`;
 
 export default App;
